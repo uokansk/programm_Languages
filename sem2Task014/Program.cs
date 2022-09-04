8ccbe17c9c5108ae4d1455ce98791fda32c87940
Aleksandr <uorkansk@yandex.ru>
2022-09-04 23:06:29 +0700

    кратность двум числам

---
+++ sem2Task014/Program.cs
@@ -0,0 +1,39 @@
+﻿//-------------------------------
+// Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+int inputNumberA = 0;
+bool Result1 = false;
+bool Result2 = false;
+
+void ReadData()
+{
+    Console.WriteLine("введите число");
+    string? inputLineA = Console.ReadLine();
+    if(inputLineA != null)
+    {
+        inputNumberA = int.Parse(inputLineA);
+    }
+}
+
+void CalculetData()
+{
+    Result1 = (inputNumberA % 7) == 0;
+    Result2 = (inputNumberA % 23) == 0;
+}
+
+void PrintData()
+{
+    if (Result1 && Result2)
+    {
+        Console.WriteLine("число кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("не кратно");
+    }
+}
+
+ReadData();
+CalculetData();
+PrintData();
