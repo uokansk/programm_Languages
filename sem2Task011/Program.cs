4a5e02e52c41a5967bba8ead641999f34b18077b
Aleksandr <uorkansk@yandex.ru>
2022-09-04 21:03:19 +0700

    со вторым вариантом

--- sem2Task011/Program.cs
+++ sem2Task011/Program.cs
@@ -1,7 +1,34 @@
 ﻿//======================================================
-// вывести 3х значное число, удалить вторую цифру
+// вывести 3х значное число, удалить вторую цифру этого числа
 //======================================================!");
 
-System.Random numberGenerator = new System.Random();
-int number = numberGenerator.Next(100, 1000);
-Console.WriteLine(number);
+// System.Random numberGenerator = new System.Random();
+// int number = numberGenerator.Next(100, 1000);
+
+// Console.WriteLine(number);
+
+// int withoutSekondDigit = (number / 100) * 10 + number % 10;
+
+// Console.WriteLine(withoutSekondDigit);
+
+// // вариант 2
+// char[] charArray = number.ToString().ToCharArray();
+// Console.WriteLine(charArray);
+// Console.WriteLine(charArray[0]);
+// Console.WriteLine(charArray[2]);
+// Console.WriteLine("" + charArray[0] + charArray[2]);
+// Console.WriteLine(charArray[0] + charArray[2]);
+
+
+// метод
+void withoutSekondDigit()
+{
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+    Console.WriteLine(number);
+
+    int withoutSekondDigit = (number / 100) * 10 + number % 10;
+    Console.WriteLine(withoutSekondDigit);
+}
+
+withoutSekondDigit();
