fb9b40abd465bd80bb9e385b7fb60c10585b107d
Aleksandr <uorkansk@yandex.ru>
2022-09-05 09:09:19 +0700

    задача 10 решена двумя способами

---
+++ sem2Task010/Program.cs
@@ -0,0 +1,53 @@
+﻿//=======================================================
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+//========================================
+
+// Console.WriteLine("введите трехзначное число");
+// string? inputLineA = Console.ReadLine();
+// if(inputLineA != null && inputLineA.Length == 3)
+//     {
+//         char[] charArray = inputLineA.ToCharArray();
+//         Console.WriteLine(charArray[1]);
+//     }
+// else
+//     {
+//         Console.WriteLine("введено не трехзначное число");
+//     }
+
+
+// вариант с методами
+char Result = ' ';
+string inputLineA = " ";
+
+void RaedData()
+    {
+        Console.WriteLine("введите трехзначное число");
+        inputLineA = Console.ReadLine();
+        // Console.WriteLine("получил" + ); 
+    }
+
+void CalculateData()
+    {
+        if(inputLineA != null && inputLineA.Length == 3)
+            {
+                char[] charArray = inputLineA.ToCharArray();
+                Result = charArray[1];    
+            }
+    }
+
+void PrintData()
+    {
+        if(inputLineA.Length == 3)
+            {
+                Console.WriteLine(Result);        
+            }
+        else
+            {
+                Console.WriteLine("введено не трехзначное число");
+            }
+    }
+
+RaedData();
+CalculateData();
+PrintData();
