1184837e287a464424d4d52a877b8aa612652cd3
Aleksandr <uorkansk@yandex.ru>
2022-09-05 09:20:59 +0700

    задача 13

---
+++ sem2Task013/Program.cs
@@ -0,0 +1,16 @@
+﻿//=================================================
+// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+//==================================================
+
+Console.WriteLine("введите число");
+string? inputLineA = Console.ReadLine();
+if(inputLineA != null && inputLineA.Length >= 3)
+    {
+        char[] charArray = inputLineA.ToCharArray();
+        Console.WriteLine("третья цифра " + charArray[2]);
+    }
+else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
