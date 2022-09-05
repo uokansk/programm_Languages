be9c0c6b66eb0bd0f1919ff4f2553c5f6596a560
Aleksandr <uorkansk@yandex.ru>
2022-09-05 21:48:41 +0700

    задача 13 метод оказался сложен

--- sem2Task013/Program.cs
+++ sem2Task013/Program.cs
@@ -3,14 +3,49 @@
 // заданного числа или сообщает, что третьей цифры нет.
 //==================================================
 
-Console.WriteLine("введите число");
-string? inputLineA = Console.ReadLine();
-if(inputLineA != null && inputLineA.Length >= 3)
+// Console.WriteLine("введите число");
+// string? inputLineA = Console.ReadLine();
+// if(inputLineA != null && inputLineA.Length >= 3)
+//     {
+//         char[] charArray = inputLineA.ToCharArray();
+//         Console.WriteLine("третья цифра " + charArray[2]);
+//     }
+// else
+//     {
+//         Console.WriteLine("третьей цифры нет");
+//     }
+
+
+string inputLine = "";
+char[] charArray = {};
+
+void ReadData()
+{
+    Console.WriteLine("введите число");
+    inputLine = Console.ReadLine();
+}
+
+void CalculateData()
+{
+    if(inputLine != null && inputLine.Length >= 3)
     {
-        char[] charArray = inputLineA.ToCharArray();
+        charArray = inputLine.ToCharArray();        
+    }
+    
+}
+
+void PrintData()
+{
+    if(charArray[2] != null)
+    {
         Console.WriteLine("третья цифра " + charArray[2]);
     }
-else
+    else
     {
         Console.WriteLine("третьей цифры нет");
     }
+}
+
+ReadData();
+CalculateData();
+PrintData();
