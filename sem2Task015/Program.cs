e6246950110375919f3f3328474b8b62e964f745
Aleksandr <uorkansk@yandex.ru>
2022-09-05 09:40:44 +0700

    задача 15 вариант для начинающих

---
+++ sem2Task015/Program.cs
@@ -0,0 +1,21 @@
+﻿//==================================================
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+//===================================================
+
+Console.WriteLine("введите первое число");
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+    {
+        int inputNumber = int.Parse(inputLine);
+        if (inputNumber == 6 || inputNumber == 7)
+            {
+                Console.WriteLine("выходной");
+            }
+        else
+            {
+                Console.WriteLine("будни");
+            }
+    }
+    
